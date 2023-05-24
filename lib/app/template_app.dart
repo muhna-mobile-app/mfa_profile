8be980003cf6cdc10e1307324016527e9ff0ae7b
kaio-nink <kaionink@gmail.com>
2023-05-24 11:34:28 -0300

    feat: add ProfileApp and export mfa_profile package

--- lib/app/template_app.dart
+++
@@ -1,4 +0,0 @@
-// import 'package:core_app/main.dart';
-// class TemplateApp implements MicroApp {
-// Add MicroApp contract implementation
-// }
--- lib/mfa_template.dart
+++
@@ -1,4 +0,0 @@
-library mfa_template;
-
-// Add your exports here
-// export 'app/template_app.dart';
